aa8d8595fe757ac0ffcc4c28f3f08297f3d2eff5
psikus <pcapelastegui@gmail.com>
2014-12-20 23:47:12 -0500

    Fully scripted web generation for feats

--- scripts/Feat-csv-to-html.R
+++ scripts/Feat-csv-to-html.R
@@ -20,25 +20,26 @@ css <- readChar(css.file, file.info(css.file)$size)
 
 #Use these lines to test scripts
 #tmp1<-buildElement(feat.raw.table$Name, feat.pre.table$Name, feat.post.table$Name)
-#feat.raw.df<-feat.raw.df[1:3,]
+feat.raw.df<-feat.raw.df[1:3,]
 
 feat.htm<-buildElementApply(feat.raw.df, feat.tag.pre, feat.tag.post, df.names=setdiff(names(feat.raw.df),"Summary"))
 #write(feat.htm,feat.htm.file)
 
 
-feat.table.htm<-buildTableApply(feat.raw.table, 
-                                df.names=setdiff(names(feat.raw.table),"Text"),
+feat.table.htm<-buildTableApply(feat.raw.df, 
+                                df.names=setdiff(names(feat.raw.df),"Text"),
                                 tableClass="Feat-table")
-#write(feat.table.htm,feat.table.htm.file)
+write(feat.table.htm,feat.table.htm.file)
 
-feat.full <- paste("<html><head><title>Feat-test</title><style type=\"text/css\">",
+feat.full <- paste("<html>\r\n<head>\r\n<title>Feat-test</title>\r\n<style type=\"text/css\">",
                    css,
                    "</style></head>\r\n<body>",
                    feat.table.htm,
                    "<div class=\"Feat-List\">",
                    feat.htm,
                    "</div>",
+                   "</body></html>",
                    sep="\r\n",
                    collapse="")
 
-write(feat.full,feat.htm.file)
+writeChar(feat.full,feat.htm.file)
--- scripts/csv-to-html.R
+++ scripts/csv-to-html.R
@@ -8,17 +8,20 @@ buildElement <- function(str, pre, post)
 buildElementApply <- function (df,pre,post, df.names=names(df))
 {
   tmp<-ldply(df.names,df,pre,post,.fun=function(n,df,pre,post){buildElement(df[[n]],pre[[n]],post[[n]])})
-  tmp<-laply(tmp,paste0, collapse="")
-  paste(pre$Body,tmp,post$Body, "\n",collapse="")
+  tmp<-laply(tmp,paste,sep="\r\n", collapse="\r\n")
+  paste(pre$Body,tmp,post$Body, "\r\n",collapse="")
 }
 
 buildTableApply <- function (df, df.names=names(df), tableClass=NULL)
 {
+  tmp1<-paste0(laply(df.names, .fun=function(n){buildElement(n,"<td>","</td>")}),collapse="")
+  tmp1<-paste0("<tr>",tmp1,"</tr>", "\n",collapse="")
   tmp<-ldply(df.names,df,.fun=function(n,df){buildElement(df[[n]],"<td>","</td>")})
   tmp<-laply(tmp,paste0, collapse="")
   tmp<-paste0("<tr>",tmp,"</tr>", "\n",collapse="")
+  tmp<-paste0(tmp1,tmp,collapse="\r\n")
   tableTag<-"<table>"
-  if(!is.null(tableClass)) {tableTag<-paste0("<table class=",tableClass,">")}
-  tmp<-paste(tableTag,tmp,"</table>", "\n",collapse="")
+  if(!is.null(tableClass)) {tableTag<-paste0("<table class=\"",tableClass,"\">")}
+  tmp<-paste(tableTag,"\r\n<tbody>" ,tmp,"</tbody>\r\n</table>", "\n",collapse="")
 }
 
