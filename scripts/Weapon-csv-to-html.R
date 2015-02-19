8b56665ad867503214ed9353ad26651714818b2c
psikus <pcapelastegui@gmail.com>
2015-02-19 13:55:33 -0500

    Updates to weapons, implements

--- scripts/Weapon-csv-to-html.R
+++ scripts/Weapon-csv-to-html.R
@@ -7,9 +7,12 @@ css <- readChar(css.file, file.info(css.file)$size)
 weapons.table.htm.file <- file.path(basedir,"html","CharacterCreation","Weapons-table.html")
 armor.table.htm.file <- file.path(basedir,"html","CharacterCreation","Armor-table.html")
 armorbyclass.table.htm.file <- file.path(basedir,"html","CharacterCreation","Armor-by-class-table.html")
+implement.table.htm.file <- file.path(basedir,"html","CharacterCreation","Implement-table.html")
 
 weapons.table <- read.csv(file.path(basedir,"raw","Weapons-raw.csv"), sep=";")%>% tbl_df()
 armor.table <- read.csv(file.path(basedir,"raw","Armor-raw.csv"), sep=";")%>% tbl_df()
+implement.table <- read.csv(file.path(basedir,"raw","Implement-raw.csv"), sep=";")%>% tbl_df()
+names(armor.table) <- gsub("\\."," ",names(armor.table))
 armorbyclass.table <- read.csv(file.path(basedir,"raw","Armor-by-class.csv"), sep=";")%>% tbl_df() %>%
   arrange(Class)
 
@@ -19,16 +22,16 @@ armorbyclass.table <- read.csv(file.path(basedir,"raw","Armor-by-class.csv"), se
 
 
 weapons.table.htm<-llply(weapons.table %>%  
-                        group_by(Category) %>% 
-                        split(weapons.table$Category)  ,
+                        group_by(Training) %>% 
+                        split(weapons.table$Training)  ,
                       .fun=buildTableApply,
                       df.names=names(weapons.table),
-                      tableClass="Feat-table")
+                      tableClass="General-table")
 
-armor.table.htm  <- buildTableApply(armor.table,tableClass="Feat-table")
-armorbyclass.table.htm  <- buildTableApply(armorbyclass.table,tableClass="Feat-table")
+armor.table.htm  <- buildTableApply(armor.table,tableClass="General-table")
+armorbyclass.table.htm  <- buildTableApply(armorbyclass.table,tableClass="General-table")
 
-
+implement.table.htm  <- buildTableApply(implement.table,tableClass="General-table")
 
 weapons.table.htm<-paste(weapons.table.htm,collapse="<br> ")
 
@@ -56,10 +59,20 @@ armorbyclass.table.htm <- paste("<html>\r\n<head>\r\n<title>Armor</title>\r\n<st
                          sep="\r\n",
                          collapse="")
 
+implement.table.htm <- paste("<html>\r\n<head>\r\n<title>Armor</title>\r\n<style type=\"text/css\">",
+                         css,
+                         "</style></head>\r\n<body>",
+                         implement.table.htm,
+                         "</body></html>",
+                         sep="\r\n",
+                         collapse="")
 
 write(weapons.table.htm,weapons.table.htm.file)
 write(armor.table.htm,armor.table.htm.file)
 write(armorbyclass.table.htm,armorbyclass.table.htm.file)
+write(implement.table.htm,implement.table.htm.file)
 
-#exploration, delete this
+#exploration, delete this.
+require(ggplot2)
 ggplot(armorbyclass.table)+aes(x=Expected.AC,fill=Role)+geom_bar(binwidth=1,color="black")
+ggplot(armorbyclass.table)+aes(x=Expected.AC,fill=Armor.Type)+geom_bar(binwidth=1,color="black")+facet_grid(Role ~.)
