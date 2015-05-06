25811e306ba8385472d41183561e8a7824239cd5
psikus <pcapelastegui@gmail.com>
2015-05-06 13:23:17 -0400

    Pdf: adjusting css width

--- scripts/html-to-pdf.R
+++ scripts/html-to-pdf.R
@@ -2,10 +2,8 @@
 
 require(knitr)
 
-basedir <- "C:/Users/acer/Documents/Perico/OCIO/SFRPG-web"
-tmpfile<- file.path(basedir,"raw","0-chapters","CharacterBuilding.Rmd")
 
-pandoc(tmpfile, format='html')
+
 
 basedir <- "C:/Users/acer/Documents/Perico/OCIO/SFRPG-web"
 tmpfile<- file.path(basedir,"raw","0-chapters","CharacterBuilding.html")
@@ -16,8 +14,7 @@ tmpfile2<- file.path(basedir,"raw","0-chapters","CharacterBuilding.pdf")
 tmpfile<- file.path(basedir,"raw","0-chapters","Feats.html")
 tmpfile2<- file.path(basedir,"raw","0-chapters","Feats.pdf")
 
-pandoc(tmpfile, format='pdf')
-pandoc(tmpfile, format='pdf')
+
 
  
 wkhtmltopdf  <- "D:\\Programas\\wkhtmltopdf\\bin\\wkhtmltopdf.exe"
@@ -25,4 +22,3 @@ wkhtmltopdf  <- "D:\\Programas\\wkhtmltopdf\\bin\\wkhtmltopdf.exe"
 system(paste(wkhtmltopdf, tmpfile, tmpfile2))
 
 
-system()
