a93c74e5f6fe5dee265b691594d19f237baca765
Pedro Capelastegui <pcapelastegui@gmail.com>
2015-11-27 00:01:50 +0000

    New Version
    Aggregated chapters 1 and 2 into 2 larger pdfs

--- scripts/2-01-Rmd-to-pdf.R
+++ scripts/2-01-Rmd-to-pdf.R
@@ -1,6 +1,9 @@
 require(knitr)
 
 basedir  <- getwd()
+.Platform$file.sep <- "\\"
+
+dir.htmpdf <- file.path('C:','\"Program Files\"','wkhtmltopdf','bin')
 
 dir.rmd  <-  file.path(basedir,"Rmd")
 rmd.files <- list()
@@ -9,22 +12,30 @@ rmd.files$a2.charbuild<- file.path(dir.rmd,"1-2-CharacterBuilding.Rmd")
 rmd.files$a3.combat<- file.path(dir.rmd,"1-3-Combat.Rmd")
 rmd.files$b2.classes<- file.path(dir.rmd,"2-1-Classes.Rmd")
 rmd.files$b3.feats<- file.path(dir.rmd,"2-2-Feats.Rmd")
+rmd.files$a.rules<- file.path(dir.rmd,"1-SFRPG-Game-Rules.Rmd")
+rmd.files$b.charbuild <- file.path(dir.rmd,"2-CharacterBuilding.Rmd")
 
-dir.rmd  <-  file.path(basedir,"Rmd")
-md.files  <- list()
-md.files$a1.intro<- file.path(dir.rmd,"1-1-Introduction.md")
-md.files$a2.charbuild<- file.path(dir.rmd,"1-2-CharacterBuilding.md")
-md.files$a3.combat<- file.path(dir.rmd,"1-3-Combat.md")
-md.files$b2.classes<- file.path(dir.rmd,"2-1-Classes.md")
-md.files$b3.feats<- file.path(dir.rmd,"2-2-Feats.md")
+
+# dir.rmd  <-  file.path(basedir,"Rmd")
+# md.files  <- list()
+# md.files$a1.intro<- file.path(dir.rmd,"1-1-Introduction.md")
+# md.files$a2.charbuild<- file.path(dir.rmd,"1-2-CharacterBuilding.md")
+# md.files$a3.combat<- file.path(dir.rmd,"1-3-Combat.md")
+# md.files$b2.classes<- file.path(dir.rmd,"2-1-Classes.md")
+# md.files$b3.feats<- file.path(dir.rmd,"2-2-Feats.md")
+# md.files$a.rules<- file.path(dir.rmd,"1-SFRPG-Game-Rules.md")
+# md.files$b.charbuild <- file.path(dir.rmd,"2-CharacterBuilding.md")
+
 
 dir.html  <-  file.path(basedir,"html")
 htm.files  <- list()
 htm.files$a1.intro<- file.path(dir.html,"Introduction.html")
 htm.files$a2.charbuild<- file.path(dir.html,"CharacterBuilding.html")
-htm.files$a3.combat<- file.path(dir.rmd,"Combat.html")
+htm.files$a3.combat<- file.path(dir.html,"Combat.html")
 htm.files$b2.classes<- file.path(dir.html,"Classes.html")
 htm.files$b3.feats<- file.path(dir.html,"Feats.html")
+htm.files$a.rules<- file.path(dir.html,"1-SFRPG-Game-Rules.html")
+htm.files$b.charbuild <- file.path(dir.html,"2-CharacterBuilding.html")
 
 dir.pdf  <- file.path(basedir,"pdf")
 pdf.files  <- list()
@@ -33,16 +44,21 @@ pdf.files$a2.charbuild<- file.path(dir.pdf,"A2-CharacterBuilding.pdf")
 pdf.files$a3.combat<- file.path(dir.pdf,"A3-Combat.pdf")
 pdf.files$b2.classes<- file.path(dir.pdf,"B2-Classes.pdf")
 pdf.files$b3.feats<- file.path(dir.pdf,"B3-Feats.pdf")
-
+pdf.files$a.rules<- file.path(dir.pdf,"1-SFRPG-Game-Rules.pdf")
+pdf.files$b.charbuild <- file.path(dir.pdf,"2-CharacterBuilding.pdf")
 
 toPdf <- function(f.in,f.out){
-  system(paste("wkhtmltopdf","--zoom 1.25 ", f.in, f.out))
+  system(paste(file.path(dir.htmpdf,"wkhtmltopdf"),"toc --zoom 1.25 ", f.in, f.out))
 }
 
 
 mapply(input=rmd.files,output_file=htm.files,FUN = rmarkdown::render)
 
 mapply(f.in=htm.files,f.out=pdf.files,FUN = toPdf)
+
+toPdf(paste (htm.files, collapse=" "), file.path(dir.pdf,"SFRPG-full.pdf"))
+
+
 
 
 
