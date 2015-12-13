eba5a691e92659438deb10bea1ed2e24cb1abc1e
Pedro Capelastegui <pcapelastegui@gmail.com>
2015-12-13 16:29:04 +0000

    Update: races and skills

--- scripts/1-01-Race-csv-to-html.R
+++ scripts/1-01-Race-csv-to-html.R
@@ -1,148 +1,151 @@
 
-getClassList <- function (basedir=getwd())
+getRaceList <- function (basedir=getwd())
 {
   require(rutils)
   source(file.path(basedir,"scripts","0-00-csv-to-html.R"))
 
-power.raw <- file.path(basedir,"raw","charactercreation","Class-Powers-raw.csv")
-power.tag <- file.path(basedir,"raw","charactercreation","Powers-tags.csv")
+#File paths
+## Race powers
+race.power.raw <- file.path(basedir,"raw","charactercreation","Race-Powers-raw.csv")
+race.power.tag <- file.path(basedir,"raw","charactercreation","Race-Powers-tags.csv")
 css.file <- file.path(basedir,"Rmd","SFRPG.css")
+## Race stats
+race.stat.raw <- file.path(basedir,"raw","charactercreation","Race-stats.csv")
+race.stat.htm.file  <- file.path(basedir,"html","CharacterCreation","Race-stats.html")
+## Race features
+race.features.raw <- file.path(basedir,"raw","charactercreation","Race-features.csv")
+race.features.tag <- file.path(basedir,"raw","charactercreation","Race-features-tags.csv")
+## Race html
+race.power.htm.file <- file.path(basedir,"html","CharacterCreation","race-Powers.html")
+race.power.table.htm.file <- file.path(basedir,"html","CharacterCreation","race-Powers-table.html")
 
-class.stat.raw <- file.path(basedir,"raw","charactercreation","Race-stats.csv")
-class.stat.htm.file  <- file.path(basedir,"html","CharacterCreation","Race-stats.html")
-
-class.features.raw <- file.path(basedir,"raw","charactercreation","Race-features.csv")
-class.features.tag <- file.path(basedir,"raw","charactercreation","Race-features-tags.csv")
-
-power.htm.file <- file.path(basedir,"html","CharacterCreation","Class-Powers.html")
-power.table.htm.file <- file.path(basedir,"html","CharacterCreation","Class-Powers-table.html")
-
-class.stat.df  <- read.csv(class.stat.raw, sep=";", header=TRUE)
-class.feature.df  <- read.csv(class.features.raw, sep=";", header=TRUE)
-
-class.stat.list  <- split(class.stat.df,class.stat.df$Class)
-class.stat.list <- llply(class.stat.list, .fun=function(a){a <- refactor(a);split(a,a$Build)})
-class.stat.list <- llply.n(class.stat.list,2,
+#Data frames
+race.stat.df  <- read.csv(race.stat.raw, sep=";", header=TRUE)
+race.feature.df  <- read.csv(race.features.raw, sep=";", header=TRUE)
+#Lists
+race.stat.list  <- split(race.stat.df,race.stat.df$Race)
+race.stat.list <- llply(race.stat.list, .fun=function(a){a <- refactor(a);split(a,a$Subrace)})
+race.stat.list <- llply.n(race.stat.list,2,
                        .fun2=function(df){
                          table <- cbind(gsub("\\."," ",names(df)),trans_df(df))
-                         htm <- buildTableApply(table,tableClass="Class-table", skipHeader = TRUE)
+                         htm <- buildTableApply(table,tableClass = "Race-table", skipHeader = TRUE)
                          list(stats=table,stats.htm=htm)
                               })
 
 usageOrder  <- c("","At-Will","Encounter","Daily")
-power.tag.df <- read.csv(power.tag, sep=";", colClasses="character")
-power.tag.pre<- power.tag.df[1,]
-power.tag.post<- power.tag.df[2,]
+race.power.tag.df <- read.csv(race.power.tag, sep=";", colClasses = "character")
+race.power.tag.pre<- race.power.tag.df[1,]
+race.power.tag.post<- race.power.tag.df[2,]
 
-feature.tag.df <- read.csv(class.features.tag, sep=";", colClasses="character")
+feature.tag.df <- read.csv(race.features.tag, sep=";", colClasses="character")
 feature.tag.pre<- feature.tag.df[1,]
 feature.tag.post<- feature.tag.df[2,]
 
-power.raw.df <- read.csv(power.raw, sep=";") %>% 
+race.power.raw.df <- read.csv(race.power.raw, sep=";") %>% 
   gsubColwise("\\n","<br>")%>% 
   tbl_df() %>% 
   mutate(UsageLimit=factor(UsageLimit, 
                            levels=c(usageOrder,setdiff(UsageLimit,usageOrder)))) %>%  
-  arrange(Class, isFeature!="Feature", Type, UsageLimit, Level, Name) %>% 
+  arrange(Race, isFeature!="Feature", Type, UsageLimit, Level, Name) %>% 
   mutate(usageColors=revalue(UsageLimit,replace=c(Daily="gray", Encounter="red","At-Will"="green")))
 
-levels(power.raw.df$usageColors)[levels(power.raw.df$usageColors)==""] <- "green"
+levels(race.power.raw.df$usageColors)[levels(race.power.raw.df$usageColors)==""] <- "green"
 
-power.raw.df  <- power.raw.df %>% 
+race.power.raw.df  <- race.power.raw.df %>% 
   mutate(Name=paste("<span class=\"",usageColors,"\">",Name, sep="")) %>%
   mutate(Level=paste(Level,"</span>",sep=""))
 
 
-class.power.list  <- split(power.raw.df,power.raw.df$Class) 
-class.power.list <- llply(class.power.list, .fun=function(a){a <- refactor(a);split(a,a$Build)})
-class.power.list <- llply.n(class.power.list,2,power.tag.pre, power.tag.post,
-                           .fun2=function(df,power.tag.pre, power.tag.post){
-                             htm <- buildElementApply(df,power.tag.pre, power.tag.post,
-                                                      df.names=setdiff(names(df),c("Summary","Build","usageColors")),
+race.race.power.list  <- split(race.power.raw.df,race.power.raw.df$Race) 
+race.race.power.list <- llply(race.race.power.list, .fun=function(a){a <- refactor(a);split(a,a$Subrace)})
+race.race.power.list <- llply.n(race.race.power.list,2,race.power.tag.pre, race.power.tag.post,
+                           .fun2=function(df,race.power.tag.pre, race.power.tag.post){
+                             htm <- buildElementApply(df,race.power.tag.pre, race.power.tag.post,
+                                                      df.names=setdiff(names(df),c("Summary","Subrace","usageColors")),
                                                       skipEmpty = TRUE)
-                             htm <- paste("<div class=\"Power-List\">",htm,"</div>" ,sep="")
+                             htm <- paste("<div race=\"Power-List\">",htm,"</div>" ,sep="")
 
                              table <- buildTableApply(df,
-                                                    df.names=c("Name", "Class", "Level", "Type","UsageLimit","Range","Action","Summary"),
-                                                    tableClass="Power-table")
+                                                    df.names=c("Name", "Race", "Level", "Type","UsageLimit","Range","Action","Summary"),
+                                                    tableClass = "Power-table")
                              list(powers=df,powers.htm=htm,powers.table=table)
                            })
 
 
-class.power.list  <- llply.n(class.power.list,2,
+race.race.power.list  <- llply.n(race.race.power.list,2,
                              .fun=function(l){
-                               l$classbuild  <- paste("<h2>",l$powers$Class[1],l$powers$Build[1],"</h2>",sep=" ")
+                               l$racebuild  <- paste("<h2>",l$powers$Race[1],l$powers$Subrace[1],"</h2>",sep=" ")
                                l})
 
 
-class.feature.list  <- split(class.feature.df,class.feature.df$Class) 
-class.feature.list <- llply(class.feature.list, .fun=function(a){a <- refactor(a);split(a,a$Build)})
-class.feature.list <- llply.n(class.feature.list,2,feature.tag.pre, feature.tag.post,
+race.feature.list  <- split(race.feature.df,race.feature.df$Race) 
+race.feature.list <- llply(race.feature.list, .fun=function(a){a <- refactor(a);split(a,a$Subrace)})
+race.feature.list <- llply.n(race.feature.list,2,feature.tag.pre, feature.tag.post,
                             .fun2=function(df,feature.tag.pre, feature.tag.post){
                               htm <- buildElementApply(df,feature.tag.pre, feature.tag.post,
-                                                       df.names=setdiff(names(df),c("Class","Build")),
+                                                       df.names=setdiff(names(df),c("race","Build")),
                                                        skipEmpty = TRUE)
-                              #htm <- paste("<div class=\"Power-List\">",htm,"</div>" ,sep="")
+                              #htm <- paste("<div race=\"Power-List\">",htm,"</div>" ,sep="")
                               
                               list(features=df,features.htm=htm)
                             })
 
 
 
-#join all class nested lists
-class.list  <- llply.parallel.multilist(class.power.list, 
-                                        list(class.power.list,class.stat.list,class.feature.list),
+#join all race nested lists
+race.list  <- llply.parallel.multilist(race.race.power.list, 
+                                        list(race.race.power.list,race.stat.list,race.feature.list),
                                         n=2,
                                         .fun=function(...){unlist(c(...),recursive=FALSE)})
 
 
 
-class.list
+race.list
 }
 
-writeClassList  <- function(class.list)
+writeraceList  <- function(race.list)
 {
   
   css.file <- file.path(basedir,"Rmd","SFRPG.css")
 
 
-class.stat.htm.file  <- file.path(basedir,"html","CharacterCreation","Class-stats.html")
+race.stat.htm.file  <- file.path(basedir,"html","CharacterCreation","race-stats.html")
 
 
 
-power.htm.file <- file.path(basedir,"html","CharacterCreation","Powers.html")
-power.table.htm.file <- file.path(basedir,"html","CharacterCreation","Powers-table.html")
+race.power.htm.file <- file.path(basedir,"html","CharacterCreation","Powers.html")
+race.power.table.htm.file <- file.path(basedir,"html","CharacterCreation","Powers-table.html")
 
 #Build power tables
 #add stuff to nested list
-#class.list  <- llply.n(class.list,2,.fun2 = function(...){c(...,a="A")})
+#race.list  <- llply.n(race.list,2,.fun2 = function(...){c(...,a="A")})
 # No longer used, keep in case we need a global power table
-# power.table.htm<-buildTableApply(power.raw.df, 
-#                       df.names=c("Name", "Class", "Level", "Type","UsageLimit","Range","Action","Summary"),
-#                       tableClass="Power-table")
-# power.table.htm<-paste(power.table.htm,collapse="<br> ")
-#write(power.table.htm,power.table.htm.file)
+# race.power.table.htm<-buildTableApply(race.power.raw.df, 
+#                       df.names=c("Name", "race", "Level", "Type","UsageLimit","Range","Action","Summary"),
+#                       tablerace="Power-table")
+# race.power.table.htm<-paste(race.power.table.htm,collapse="<br> ")
+#write(race.power.table.htm,race.power.table.htm.file)
 
 #Build full text descriptions
-power.full <- paste("<html>\r\n<head>\r\n<title>power-test</title>\r\n<style type=\"text/css\">",
+race.power.full <- paste("<html>\r\n<head>\r\n<title>power-test</title>\r\n<style type=\"text/css\">",
                    css,
                    "</style></head>\r\n<body>",
-                   "<p>",class.list$Fighter$Guardian$stats.htm,"</p>",
-                   "<p>",class.list$Fighter$Guardian$powers.table,"</p>",
-                   class.list$Fighter$Guardian$powers.htm,
+                   "<p>",race.list$Human$Human$stats.htm,"</p>",
+                   "<p>",race.list$Human$Human$powers.table,"</p>",
+                   race.list$Human$Human$powers.htm,
                    "</body></html>",
                    sep="\r\n",
                    collapse="")
 
 
-tmp <- llply.n(class.list,2,
+tmp <- llply.n(race.list,2,
                .fun=function(l){
-                 paste("<p>",l$classbuild,"</p>\r\n",
-                       "<p><h3>Class Stats</h3></p>\r\n",
+                 paste("<p>",l$racebuild,"</p>\r\n",
+                       "<p><h3>race Stats</h3></p>\r\n",
                        "<p>",l$stats.htm,"</p>\r\n",
-                       "<p><h3>Class features</h3></p>\r\n",
+                       "<p><h3>race features</h3></p>\r\n",
                        "<p>",l$features.htm,"</p>\r\n",
-                       "<p><h3>Class Powers</h3></p>\r\n",
+                       "<p><h3>race Powers</h3></p>\r\n",
                        #"<p>",l$powers.table,"</p>\r\n",
                        "<p>",l$powers.htm,"</p>\r\n",
                        sep="")
@@ -151,7 +154,7 @@ tmp <- llply.n(class.list,2,
 
 tmp <- paste(llply(tmp,collapse="",.fun=paste),collapse="\r\n")
 
-power.full <- paste("<html>\r\n<head>\r\n<title>power-test</title>\r\n<style type=\"text/css\">",
+race.power.full <- paste("<html>\r\n<head>\r\n<title>class-power-test</title>\r\n<style type=\"text/css\">",
                     css,
                     "</style></head>\r\n<body>",
                     tmp,
@@ -162,6 +165,6 @@ power.full <- paste("<html>\r\n<head>\r\n<title>power-test</title>\r\n<style typ
 
 
 
-writeChar(power.full,power.htm.file)
-classList
+writeChar(race.power.full,race.power.htm.file)
+raceList
  }
