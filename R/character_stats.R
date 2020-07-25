f874a6d5802da674686172ba8f3c64eb81b1e4c8
capelastegui <pcapelastegui@gmail.com>
2020-07-25 11:45:04 +0100

    feature #134; New Class Builds

--- R/character_stats.R
+++ R/character_stats.R
@@ -1,3 +1,7 @@
+#' Generate HP table for Player Characters
+#'
+#'
+#' @return DataFrame with HP and Surge Values per level
 #' @export
 get_df_hp <- function() {
   df_hp <- tibble::tibble(Level = 1:30,
@@ -17,6 +21,25 @@ get_df_hp <- function() {
   df_hp
 }
 
+#' Generate HP table for Beast Companions
+#'
+#'
+#' @return DataFrame with HP per level
+#' @export
+get_df_hp_beast <- function() {
+  df_hp <- tibble::tibble(Level = 1:30,
+                          hp_level=c(1:10+10, (1:10+10)*2, (1:10+10)*4)) %>%
+    dplyr::mutate(HP1=(1.5*hp_level) %>% floor(),
+                  HP2=(2*hp_level) %>% floor()) %>%
+    #floor() %>% # TODO: Apply floor to all columns
+    dplyr::select(Level,HP1,  HP2,)
+  df_hp
+}
+
+#' Generate html with table for PC HP
+#'
+#'
+#' @return html table
 #' @export
 get_df_hp_html <- function(df_hp=NULL) {
 
@@ -27,6 +50,21 @@ get_df_hp_html <- function(df_hp=NULL) {
                  col.names=c('Level',rep(c('HP','Surge'),3))) %>%
     kableExtra::add_header_above(
       c(" ", "Low HP"=2, "Medium HP"=2, "High HP"=2))
+
+  df_hp_html
+}
+
+#' Generate html with table for Beast Companion HP
+#'
+#'
+#' @return html table
+#' @export
+get_df_hp_html_beast <- function(df_hp_beast=NULL) {
+
+  if(df_hp_beast %>% is.null()) {df_hp_beast = get_df_hp_beast()}
+
+  df_hp_html <- df_hp_beast %>%
+    knitr::kable(format='html', col.names=c('Level','Medium HP','High HP'))
 
   df_hp_html
 }
